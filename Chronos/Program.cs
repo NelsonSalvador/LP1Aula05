03e7436d9eaa394d73c3037130b0d7a50fbc8bb6
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-04-09 12:38:15 +0100

    Ex_5_Chronos

---
+++ Chronos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variaveis 
+            Stopwatch cron1, cron2;
+
+            // Inicializar cronometros
+            cron1 = new Stopwatch();
+            cron2 = new Stopwatch();
+
+            cron1.Start();
+            Thread.Sleep(1000);
+
+            cron2.Start();
+            Thread.Sleep(1000);
+
+            cron1.Stop();
+            cron2.Stop();
+
+            Console.WriteLine($"Tempo cron1 = {cron1.ElapsedMilliseconds}");
+            Console.WriteLine($"Tempo cron1 = {cron2.ElapsedMilliseconds}");
+        }
+    }
+}
