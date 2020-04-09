6beb4960161fe4754d5081b983f4dce2f2705cf9
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-04-09 12:54:01 +0100

    Lion

---
+++ MyTestOfRandoms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyTestOfRandoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aux;
+            int n, soma = 0;
+            Random rnd = new Random();
+
+            Console.Write("Insira numero de dados: ");
+            aux = Console.ReadLine();
+            n = int.Parse(aux);
+
+            for (int i = 0; i < n; i++)
+            {
+                soma += rnd.Next(1, 7);
+            }
+            Console.WriteLine($"Soma {soma}");
+        }
+    }
+}
